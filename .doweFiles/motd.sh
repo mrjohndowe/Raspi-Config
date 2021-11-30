2af457ba226478ce0f26faaae381f1e996c4550c
mrjohndowe <r.davila.jose@icloud.com>
2021-11-30 10:04:46 -0700

    Update motd.sh and .wget-hsts

---
+++ .doweFiles/motd.sh
@@ -0,0 +1,30 @@
+# Message Of The Day
+# IP Address
+# IP=`ifconfig wlan0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1`
+
+IP=`hostname -I`;
+EXT_IP=`wget -q -O - http://icanhazip.com/ | tail`;
+RASPI_MODEL=`dmesg | grep "Machine model:" | cut -d ":" -f 2`;
+
+welcome() {
+  echo "Bonjour!";
+}
+
+# Identify Pi version
+# http://elinux.org/RPi_HardwareHistory
+
+version() {
+  echo "This is a" ${RASPI_MODEL};
+}
+
+# Show internal and external IP address
+
+ip() {
+  echo ${IP};
+  echo ${EXT_IP};
+}
+
+# Execute
+welcome
+version 
+ip
