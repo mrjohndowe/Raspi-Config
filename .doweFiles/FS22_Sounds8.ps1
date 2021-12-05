76faf1a211673ab231b37c4541ba49fcd4751ed0
mrjohndowe <r.davila.jose@icloud.com>
2021-12-05 01:53:32 -0700

    Update FS22_Sounds8.ps1 and WinSCP.ini

---
+++ .doweFiles/FS22_Sounds8.ps1
@@ -0,0 +1,15 @@
+﻿$file = "D:\SteamLibrary\steamapps\common\Farming Simulator 22\data\sounds\soundTemplates.xml"
+$increaseby = 8
+
+if(-not (Test-Path "$file.bak")) {
+    Copy-Item $file "$file.bak"
+}
+
+[xml]$template = Get-Content $file
+
+$template | Select-Xml -XPath '//volume' | ForEach-Object {
+    $_.node.indoor = ([decimal]($_.node.indoor)+$increaseby).ToString().Replace(",",".")
+    $_.node.outdoor = ([decimal]($_.node.outdoor)+$increaseby).ToString().Replace(",",".")
+}
+
+$template.Save($file)
